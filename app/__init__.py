43dacac13eead029e7e5d3c6296fd574ac99674b
Noktoborus <noktoborus@gmail.com>
2019-08-04 00:04:40 +0300

    Добавлены комментарии и режим старых планшетов

--- app/__init__.py
+++ app/__init__.py
@@ -23,18 +23,31 @@ def ok():
 def laps():
     newlist = []
     for el in laps_data.values():
+        # Список стартов для всех устройств: <LapId: int, CrewNumber: int, LapNumber: int>
         newlist.append({'LapId': el['LapId'],
                         'LapNumber': el['LapNumber'],
                         'CrewNumber': el['CrewNumber']})
     print(json_serialize(newlist))
     return json_serialize(newlist)
 
+# Переключение между старым режимом работы и новым:
+# server_make_key = True -> для включения старого режима работы
+#   В этом случае, сервер генерирует идентификторы заездов для последущего взаимодействия всех остальных режимов
+# server_make_key = False -> новый режим работы
+#   Ожидается, что генерировать идентификаторы будет стартовый планшет
+server_make_key = True
+#
 
 @app.route('/api/laps/updatelaps', methods=['POST'])
 def updatelaps():
+    # Получение значение от судьи на старте
     list_data = json_extract(request.data)
     for el in list_data:
-        no = el['LapId']
+        if server_make_key:
+            no = len(laps_data) + 1;
+            el['LapId'] = no
+        else:
+            no = el['LapId']
         laps_data[no] = el
         print("Update record '%s': %s" % (no, el))
     return "true"
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # vim: ft=sh ff=unix fenc=utf-8
 # file: run.sh
+export FLASK_ENV=development
 
-flask run
+flask run --port 9000
 

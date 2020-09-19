c6346e77e978ff633c4e7c63fe1d6f31752cef79
Noktoborus <noktoborus@gmail.com>
2020-09-19 21:01:37 +0300

    remove web ui from this repository

--- main.go
+++ main.go
@@ -14,17 +14,6 @@ func main() {
   r.HandleFunc("/api/data/{CompetitionId:[0-9]+}/{TimeStamp:[0-9]+}/{TerminalString:[0-9a-fA-F]+}", GetDataHandler).Methods("POST")
   r.HandleFunc("/api/update/{CompetitionId:[0-9]+}/{TerminalString:[0-9a-fA-F]+}", UpdateHandler).Methods("POST")
 
-  fs := http.FileServer(http.Dir("res/"));
-
-  r.HandleFunc("/", IndexHandler).Methods("GET")
-  r.HandleFunc("/index.html", IndexHandler).Methods("GET")
-  r.Handle("/favicon.ico", fs).Methods("GET");
-  r.Handle("/js/{File}", fs).Methods("GET")
-  r.Handle("/css/{File}", fs).Methods("GET")
-
-  r.Handle("/app/", fs)
-  r.Handle("/app/{File}", fs)
-
   r.HandleFunc("/api/admin/competitions/{TerminalString:[0-9a-fA-F]+}", AdminListHandler).Methods("GET")
   r.HandleFunc("/api/admin/competition/set/{CompetitionId:[0-9]+}/{TerminalString:[0-9a-fA-F]+}", AdminHandler).Methods("POST")
 
